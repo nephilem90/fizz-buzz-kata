6bbf226bb44d5527ca0e21714797920e686ccbda
furt <nephilem90@gmail.com>
2019-06-09 19:41:22 +0200

    step 2, fizz buzz for nuber contain 3 or 5

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,24 +1,29 @@
 class FizzBuss:
 
-    def __init__(self, number):
-        self.number = number
-
-    def return_number_or_fizz_buzz(self):
-        return_value = self.number
-        if self.is_fizz() and self.is_buzz():
+    def return_number_or_fizz_buzz(self, number):
+        return_value = number
+        if self.is_fizz(number) and self.is_buzz(number):
             return_value = 'fizzbuzz'
-        elif self.is_fizz():
+        elif self.is_fizz(number):
             return_value = 'fizz'
-        elif self.is_buzz():
+        elif self.is_buzz(number):
             return_value = 'buzz'
         return return_value
 
-    def is_fizz(self):
-        if (self.number % 3) == 0:
+    def is_fizz(self, number):
+        return self.contain(number, 3) or self.is_multiple(number, 3)
+
+    def is_buzz(self, number):
+        return self.is_multiple(number, 5) or self.contain(number, 5)
+
+    def contain(self, number, must_contains):
+        unit = number % 10
+        if number == must_contains or unit == must_contains:
             return True
+        elif number > 10:
+            return self.contain((number - unit) / 10, must_contains)
         return False
 
-    def is_buzz(self):
-        if (self.number % 5) == 0:
-            return True
-        return False
+    @staticmethod
+    def is_multiple(number, multiple_of):
+        return (number % multiple_of) == 0
--- FizzBuzzTesst.py
+++ FizzBuzzTesst.py
@@ -5,28 +5,41 @@ from random import randint
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_tree_return_fizz(self):
-        self.assertEqual(FizzBuss(3).return_number_or_fizz_buzz(), 'fizz')
+    def test_three_return_fizz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(3), 'fizz')
 
     def test_five_return_buzz(self):
-        self.assertEqual(FizzBuss(5).return_number_or_fizz_buzz(), 'buzz')
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(5), 'buzz')
 
     def test_fifteen_is_fizzbuzz(self):
-        self.assertEqual(FizzBuss(15).return_number_or_fizz_buzz(), 'fizzbuzz')
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(15), 'fizzbuzz')
 
-    def test_all_tree_multiple_are_fizz(self):
-        self.assertEqual(FizzBuss(self.rand_multiple(3, 5)).return_number_or_fizz_buzz(), 'fizz')
+    def test_all_three_multiple_are_fizz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(self.rand_multiple(3, 'buzz')), 'fizz')
 
     def test_all_five_multiple_are_buzz(self):
-        self.assertEqual(FizzBuss(self.rand_multiple(5, 3)).return_number_or_fizz_buzz(), 'buzz')
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(self.rand_multiple(5, 'fizz')), 'buzz')
 
     def test_all_fifteen_multiple_are_fizzbuzz(self):
-        self.assertEqual(FizzBuss(randint(1111, 9999) * 15).return_number_or_fizz_buzz(), 'fizzbuzz')
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(randint(1111, 9999) * 15), 'fizzbuzz')
 
-    def rand_multiple(self, multiple_of, not_multiple_of):
-        rand_multiple = randint(1111, 9999) * multiple_of
-        if (rand_multiple % not_multiple_of) == 0:
-            return self.rand_multiple(multiple_of, not_multiple_of)
+    def test_onehundredthree_is_fizz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(103), 'fizz')
+
+    def test_onehundredthirtyfour_is_fizz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(134), 'fizz')
+
+    def test_thirteen_is_fizz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(13), 'fizz')
+
+    def test_onehundredfiftyfour_is_buzz(self):
+        self.assertEqual(FizzBuss().return_number_or_fizz_buzz(154), 'buzz')
+
+    def rand_multiple(self, number, no_type):
+        rand_multiple = randint(1111, 9999) * number
+        if (no_type == 'fizz' and FizzBuss().is_fizz(rand_multiple)) or (
+                no_type == 'buzz' and FizzBuss().is_buzz(rand_multiple)):
+            return self.rand_multiple(number, no_type)
         return rand_multiple
 
 
